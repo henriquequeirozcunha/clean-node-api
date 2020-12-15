bb7221bfe4a9ee84e8dee660cc085988d16db4d9
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-12-15 20:54:48 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -37,7 +37,7 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
-      return serverError()
+      return serverError(error)
     }
   }
 }
