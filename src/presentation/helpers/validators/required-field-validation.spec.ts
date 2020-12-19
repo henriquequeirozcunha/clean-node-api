d6bbaacda4b68927ef5855bc8234c319495ab9f3
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-12-19 18:57:56 -0300

    test: ensure RequiredFieldValidation returns a MissingParam error if validation fails

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,14 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { MissingParamError } from '../../erros'
+describe('RequiredField Validation', () => {
+  test('Should return a MissingParamError if validation fails', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+  test('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
