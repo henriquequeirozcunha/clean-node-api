0c84d8aa2b40f5a96228e0dc4ca448ac2b459670
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-12-19 19:17:33 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-field-validation.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidParamError } from './../../erros/invalid-params-error'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('CompareFieldsValidation Validation', () => {
+  test('Should return a InvalidParmError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'other_value' })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
