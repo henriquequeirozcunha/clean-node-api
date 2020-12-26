9eb1c28cd03639788028dcb5f18808d61b32a93c
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-12-25 21:27:05 -0300

    feat: ensure AccountMongoRepository returns an Account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,10 +2,17 @@
 import { AddAccount, AddAccountModel } from './../../../../domain/usecases/add-account'
 import { AccountModel } from '../../../../domain/models/account'
 import { MongoHelper } from '../helpers/mongo-helper'
-export class AccountMongoRepository implements AddAccount {
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
+export class AccountMongoRepository implements AddAccount, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getColletion('accounts')
     const result = await accountCollection.insertOne(accountData)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getColletion('accounts')
+    const account = await accountCollection.findOne({ email })
+    return account && MongoHelper.map(account)
   }
 }
