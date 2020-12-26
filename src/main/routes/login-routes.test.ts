cba350410ca7367f730ba452c9e527e79cfbb555
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-12-26 15:37:19 -0300

    test: ensure login route returns 401 on invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -46,5 +46,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on invalid credentials', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'henrique@email.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
