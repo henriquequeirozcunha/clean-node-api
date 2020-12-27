791a2f6274c02d9f9f0040d329963f5c02b39df4
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-12-27 14:10:50 -0300

    refactor: change routes to use filesystem instead of fast-glob

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
+import { Router } from 'express'
 import { makeLoginController } from './../factories/login/login-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeSignUpController } from '../factories/signup/signup-factory'
-import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
