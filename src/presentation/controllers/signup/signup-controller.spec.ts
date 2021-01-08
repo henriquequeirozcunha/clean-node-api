96d6adb8630b1d2df8b4562ae9e430a4812a049e
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-01-07 22:58:52 -0300

    feat: ensure SignUpController returns an accessToken on success

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -107,7 +107,7 @@ describe('SignUp Controller', () => {
     const { sut } = makeSut()
     const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(ok(makeFakeAccount()))
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
     // expect(httpResponse.statusCode).toBe(200)
     // expect(httpResponse.body).toEqual(makeFakeAccount())
   })
