7aadb6884b76e6c06ef1071ac68fcd0b4ab9d589
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-01-19 23:57:43 -0300

    test: ensure SurveyMongoRepository add a survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,38 @@
+import { SurveyMongoRepository } from './survey-mongo-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let surveyCollection: Collection
+
+describe('Survey MongoDB Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getColletion('surveys')
+    await surveyCollection.deleteMany({})
+  })
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+  test('Should add a survey on success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [{
+        answer: 'any_answer',
+        image: 'any_image'
+      },
+      {
+        answer: 'other_answer'
+      }]
+    })
+    const survey = await surveyCollection.findOne({
+      question: 'any_question'
+    })
+    expect(survey).toBeTruthy()
+  })
+})
