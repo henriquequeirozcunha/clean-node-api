52f36bad3db57220f092adeaf8c006533b5a50bc
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-02-28 20:52:58 -0300

    feat: ensure LoadSurveysController returns 500 if LoadSurvey use case throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,3 +1,4 @@
+import { serverError } from './../../../helpers/http/http-helper'
 import { LoadSurveys } from './../../../../domain/usecases/load-survey'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { ok } from '../../../helpers/http/http-helper'
@@ -5,7 +6,11 @@ import { ok } from '../../../helpers/http/http-helper'
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.loadSurveys.load()
-    return ok(surveys)
+    try {
+      const surveys = await this.loadSurveys.load()
+      return ok(surveys)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
