ce940950d8b4583924b09b32f7b74cf74fcb3211
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-06 18:28:02 -0300

    fix: remove .map files from production build

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,7 @@ export default (app: Express): void => {
   app.use('/api', router)
   // fg.sync('**/src/main/routes/**routes.ts').map(async (file) => (await import(`../../../${file}`)).default(router))
   readdirSync(`${__dirname}/../routes`).map(async (file: string) => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && (!file.endsWith('.map'))) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
