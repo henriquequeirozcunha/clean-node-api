fd85288b390ff3b2ed6d84b661b0895afbcac1b8
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-07 12:27:46 -0300

    feat: ensure SaveSurveyResult calls LoadSurveyById correctly and validate its response

---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.controller.ts
@@ -0,0 +1,31 @@
+import { AccessDeniedError } from '@/presentation/erros'
+import { serverError, ok, forbidden } from '@/presentation/helpers/http/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { SaveSurveySurveyResult } from '@/domain/usecases/survey-result/save-survey-result'
+import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
+
+export class SaveSurveyResultController implements Controller {
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveySurveyResult: SaveSurveySurveyResult) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const survey = await this.loadSurveyById.loadById(httpRequest.body.id)
+      if (!survey) {
+        return forbidden(new AccessDeniedError())
+      }
+      const { accountId, answer, date, surveyId } = httpRequest.body
+      await this.saveSurveySurveyResult.save({
+        accountId,
+        answer,
+        date,
+        surveyId
+      })
+
+      return ok({})
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
