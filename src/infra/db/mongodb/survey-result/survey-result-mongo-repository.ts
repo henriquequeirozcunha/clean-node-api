c55b63f5e00684bf641f41870a391aa0f1464b26
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-07 11:10:32 -0300

    feat: ensure SurveyResultMongoRepository add surveyResult if it is new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,24 @@
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getColletion('surveysResults')
+    const surveyResult = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnOriginal: false
+    })
+    console.log('surveyResult', surveyResult)
+    return surveyResult && MongoHelper.map(surveyResult.value)
+  }
+}
