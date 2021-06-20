7e04c8d62f652ed2b92c9eb5e6a0ed6dc8b54296
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-06-20 13:56:02 -0300

    feat: add swagger API

---
+++ src/main/docs/index.ts
@@ -0,0 +1,13 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: '4Dev - Enquetes para Programadores',
+    description: 'Essa é a documentação da API feita por Henrique Cunha no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
+    version: '1.0.0',
+    contact: {
+      name: 'Henrique Cunha',
+      email: 'henrique_queiroz_cunha@hotmail.com',
+      url: 'https://www.linkedin.com/in/henrique-queiroz-cunha-b610bb5b/'
+    }
+  }
+}
