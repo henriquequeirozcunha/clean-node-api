63293adc1e125571f45a8a0ed402143f55f01fe1
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-01-15 21:23:58 -0300

    refactor: remove unnecessary code

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -6,7 +6,7 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
-      return surveys?.length ? ok(surveys) : noContent()
+      return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
     }
