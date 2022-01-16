50f5ac284bca0f0469cf529c35204692310fcb06
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-01-15 21:23:15 -0300

    test: ensure SurveyResultMongoRepository returns null on load surveys without answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -137,5 +137,11 @@ describe('SurveyResult MongoDB Repository', () => {
       expect(surveyResult.answers[2].count).toBe(0)
       expect(surveyResult.answers[2].percent).toBe(0)
     })
+    test('Should return null if there is no survey result', async () => {
+      const survey = await makeSurvey()
+      const sut = makeSut()
+      const surveyResult = await sut.loadBySurveyId(survey.id)
+      expect(surveyResult).toBeNull()
+    })
   })
 })
