172f06bdff3423eca2c10c264920c61a1a09be9c
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-01-23 23:11:45 -0300

    feat: add SurveyResult GraphQL tests

--- src/main/graphql/resolvers/survey-result.ts
+++ src/main/graphql/resolvers/survey-result.ts
@@ -4,9 +4,9 @@ import { makeSaveSurveyResultController } from '@/main/factories/controllers/sur
 
 export default {
   Query: {
-    surveyResult: async (parent: any, args: any) => adaptResolver(makeLoadSurveyResultController(), args)
+    surveyResult: async (parent: any, args: any, context: any) => adaptResolver(makeLoadSurveyResultController(), args, context)
   },
   Mutation: {
-    saveSurveyResult: async (parent: any, args: any) => adaptResolver(makeSaveSurveyResultController(), args)
+    saveSurveyResult: async (parent: any, args: any, context: any) => adaptResolver(makeSaveSurveyResultController(), args, context)
   }
 }
